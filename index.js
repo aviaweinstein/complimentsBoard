d8dfbf082ebc90e32ef8e5e3d8606133da7142a2
Avia Weinstein <aviaweinstein@gmail.com>
2019-08-20 17:20:57 -0500

    set up connection to DB and WIP getting data from db at index

--- index.js
+++ index.js
@@ -1,95 +1,99 @@
-// var express = require('express');
-// var bodyParser = require('body-parser');
-// var app = express();
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
 
-// app.set('view engine', 'ejs');
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(express.static("public"));
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
-// //the wow array with initial placeholders for added wow
-// var wow = ['You are awesome', 'Thanks for your help'];
+//the wow array with initial placeholders for added wow
+var wow = ['You are awesome', 'Thanks for your help'];
 
-// app.post('/addwow', function (req, res) {
-//     var newWow = req.body.newwow;
-//     wow.push(newWow);
-//     res.redirect('/');
-// });
-
-// app.get('/', function(req, res) {
-//     res.render('index', { wow: wow });
-// });
-
-// const findDocuments = function(db, callback) {
-//     // Get the documents collection
-//     const collection = db.collection('user');
-//     // Find some documents
-//     collection.find({}).toArray(function(err, docs) {
-//         assert.equal(err, null);
-//         console.log("Found the following records");
-//         console.log(docs)
-//         callback(docs);
-//     });
-// }
-
-// const MongoClient = require('mongodb').MongoClient;
-// const assert = require('assert');
-// // Connection URL
-// const url = 'mongodb://admin:password@localhost:27017';
-// // Database Name
-// const dbName = 'wowboard';
-// // Create a new MongoClient
-// // const client = new MongoClient(url);
-
-// // Use connect method to connect to the Server
-// MongoClient.connect(url, function(err, client) {
-//     assert.equal(null, err);
-//     console.log("Connected successfully to server");
-
-//     const db = client.db(dbName);
-
-//     findDocuments(db, function() {
-//         client.close();
-//     });
-// });
-
-
-// // the server is listening on port 3000 for connections
-// app.listen(3000, function () {
-//     console.log('App listening on port 3000!')
-// });
+const findDocuments = function(db, callback) {
+    // Get the documents collection
+    const collection = db.collection('user');
+    // Find some documents
+    collection.find({}).toArray(function(err, docs) {
+        assert.equal(err, null);
+        console.log("Found the following records");
+        console.log(docs)
+        callback(docs);
+    });
+}
 
 const MongoClient = require('mongodb').MongoClient;
 const assert = require('assert');
- 
 // Connection URL
-const url = 'mongodb://localhost:27017';
- 
-// Database Name
-const dbName = 'myproject';
- 
-// Use connect method to connect to the server
+const url = 'mongodb://root:pass@localhost:27027/admin';
+
+const dbName = 'wowboard';
+
+// Use connect method to connect to the Server
 MongoClient.connect(url, function(err, client) {
-  assert.equal(null, err);
-  console.log("Connected successfully to server");
- 
-  const db = client.db(dbName);
- 
-  insertDocuments(db, function() {
-    client.close();
-  });
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
+
+    const db = client.db(dbName);
+
+    findDocuments(db, function() {
+        client.close();
+    });
+
+    app.post('/addwow', function (req, res) {
+        var newWow = req.body.newwow;
+        db.wow.insertOne({text: newWow, giver: 'aweinstein@vacayhomeconnect.com', receiver: 'zkann@vacayhomeconnect.com'});
+        res.redirect('/');
+    });
+
+    app.get('/', function(req, res) {
+        db.collection('wow').find({}).toArray(function(err, result) {
+            // if (err) {
+            //     throw err;
+            //     return res.status(400).send(err);
+            // }
+            // console.log(result);
+            res.render('index', { wow: result });
+        });
+    });
 });
 
-const insertDocuments = function(db, callback) {
-    // Get the documents collection
-    const collection = db.collection('documents');
-    // Insert some documents
-    collection.insertMany([
-      {a : 1}, {a : 2}, {a : 3}
-    ], function(err, result) {
-      assert.equal(err, null);
-      assert.equal(3, result.result.n);
-      assert.equal(3, result.ops.length);
-      console.log("Inserted 3 documents into the collection");
-      callback(result);
-    });
-  }
+// the server is listening on port 3000 for connections
+app.listen(3000, function () {
+    console.log('App listening on port 3000!')
+});
+
+// const MongoClient = require('mongodb').MongoClient;
+// const assert = require('assert');
+ 
+// // Connection URL
+// const url = 'mongodb://root:pass@localhost:27027/admin';
+ 
+// // Database Name
+// const dbName = 'myproject';
+ 
+// // Use connect method to connect to the server
+// MongoClient.connect(url, function(err, client) {
+//   assert.equal(null, err);
+//   console.log("Connected successfully to server");
+ 
+//   const db = client.db(dbName);
+ 
+//   insertDocuments(db, function() {
+//     client.close();
+//   });
+// });
+
+// const insertDocuments = function(db, callback) {
+//     // Get the documents collection
+//     const collection = db.collection('documents');
+//     // Insert some documents
+//     collection.insertMany([
+//       {a : 1}, {a : 2}, {a : 3}
+//     ], function(err, result) {
+//       assert.equal(err, null);
+//       assert.equal(3, result.result.n);
+//       assert.equal(3, result.ops.length);
+//       console.log("Inserted 3 documents into the collection");
+//       callback(result);
+//     });
+//   }
