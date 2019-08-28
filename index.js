5913d5266a13eb914f038cfa5c3bcb7f5874606c
Avia Weinstein <aviaweinstein@gmail.com>
2019-08-27 18:10:22 -0700

    Feature/setup mongo (#1)
    * simplifies code to try and isolate mongo setup issues
    * set up connection to DB and WIP getting data from db at index
    * gets index and post routes working
    * cleans up dead code

--- index.js
+++ index.js
@@ -1,26 +1,40 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var app = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
+const app = express();
+const url = 'mongodb://root:pass@localhost:27027/admin';
+const dbName = 'wowboard';
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-//the wow array with initial placeholders for added wow
-var wow = ['You are awesome', 'Thanks for your help'];
+// Use connect method to connect to the Server
+MongoClient.connect(url, function(err, client) {
+    assert.equal(null, err);
+    console.log("Connected successfully to server");
 
-app.post('/addwow', function (req, res) {
-    var newWow = req.body.newwow;
-    wow.push(newWow);
-    res.redirect('/');
-});
+    const db = client.db(dbName);
 
-app.get('/', function(req, res) {
-    res.render('index', { wow: wow });
+    app.post('/addwow', function (req, res) {
+        var newWow = req.body.newwow;
+        db.collection('wow').insertOne({text: newWow, giver: 'aweinstein@vacayhomeconnect.com', receiver: 'zkann@vacayhomeconnect.com'});
+        res.redirect('/');
+    });
+
+    app.get('/', function(req, res) {
+        db.collection('wow').find({}).toArray(function(err, result) {
+            if (err) {
+                throw err;
+                return res.status(400).send(err);
+            }
+            res.render('index', { wow: result });
+        });
+    });
 });
 
 // the server is listening on port 3000 for connections
 app.listen(3000, function () {
     console.log('App listening on port 3000!')
 });
-
