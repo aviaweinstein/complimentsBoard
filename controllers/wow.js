ace850295acb1dbc9c52b11c09ec240ac76a3dc6
Avia Weinstein <aviaweinstein@gmail.com>
2019-09-05 15:34:53 -0700

    removes commented out code

--- controllers/wow.js
+++ controllers/wow.js
@@ -1,38 +1,10 @@
 const Wow = require('../models/wow');
-const mongoose = require('mongoose');
 
 // retrieve a list of all wows
 exports.list = (req, res) => {
-	// const query = req.query || {};
-
-	// Wow.apiQuery(query)
-	// 	// // limit the information returned (server side) – e.g. no password
-	// 	// .select('name email username bio url twitter background')
-	// 	.then(wows => {
-	// 		res.json(wows);
-	// 		console.log(wows);
-	// 		// res.render('index', { wow: wows });
-	// 	})
-	// 	.catch(err => {
-	// 		res.status(422).send(err.errors);
-	// 	});
 	Wow.find({}, function (err, data) {
-		console.log(data); // it will print your collection data
 		res.render('index', { wow: data });
 	});
-	// mongoose.connection.db.collection('wows', function (err, collection) {
-	// 	// Locate all the entries using find
-	// 	collection.find().toArray(function(err, wows) {
-	// 		/* whatever you want to do with the results in node such as the following
-	// 			 res.render('home', {
-	// 				 'title': 'MyTitle',
-	// 				 'data': results
-	// 			 });
-	// 		*/
-	// 		console.log(wows);
-	// 		res.render('index', { wow: wows });
-	// 	});
-	// });
 };
 
 // create a wow
@@ -41,9 +13,8 @@ exports.post = (req, res) => {
 	console.log('posting');
 
 	Wow.create(data)
-		.then(wow => {
-			res.json(wow);
-			return res.redirect('/');
+		.then(() => {
+			res.redirect('/');
 		})
 		.catch(err => {
 			res.status(500).send(err);
