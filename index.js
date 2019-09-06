d591fcbb21555040e2d1e3ff488a2edd0eb9b213
Avia Weinstein <aviaweinstein@gmail.com>
2019-09-05 20:50:59 -0700

    feature/auth0_login (#3)
    adds auto0 for login auth

--- index.js
+++ index.js
@@ -2,45 +2,120 @@ const fs = require('fs');
 const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
-// const cors = require('cors');
-// const compression = require('compression');
-// const MongoClient = require('mongodb').MongoClient;
-// const assert = require('assert');
+var cookieParser = require('cookie-parser');
+var session = require('express-session');
+var dotenv = require('dotenv');
+var passport = require('passport');
+var Auth0Strategy = require('passport-auth0');
+var flash = require('connect-flash');
+var userInViews = require('./lib/middleware/userInViews');
+var authRouter = require('./routes/auth');
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+dotenv.config();
+// Configure Passport to use Auth0
+var strategy = new Auth0Strategy(
+    {
+        domain: process.env.AUTH0_DOMAIN,
+        clientID: process.env.AUTH0_CLIENT_ID,
+        clientSecret: process.env.AUTH0_CLIENT_SECRET,
+        callbackURL:
+        process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
+    },
+    function (accessToken, refreshToken, extraParams, profile, done) {
+        // accessToken is the token to call Auth0 API (not needed in the most cases)
+        // extraParams.id_token has the JSON Web Token
+        // profile has all the information from the user
+        return done(null, profile);
+    }
+);
+
+passport.use(strategy);
+
+// You can use this section to keep a smaller payload
+passport.serializeUser(function (user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function (user, done) {
+    done(null, user);
+});
 
 const app = express();
-// const url = 'mongodb://root:pass@localhost:27027/admin';
-// const dbName = 'wowboard';
 
-app.set('view engine', 'ejs');
-// app.use(cors());
-// app.use(compression());
+// View engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-app.use(express.static("public"));
+app.use(cookieParser());
+// config express-session
+var sess = {
+    secret: 'CHANGE THIS SECRET',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true
+};
 
-// Use connect method to connect to the Server
-// MongoClient.connect(url, function(err, client) {
-//     assert.equal(null, err);
-//     console.log("Connected successfully to server");
+if (app.get('env') === 'production') {
+    sess.cookie.secure = true; // serve secure cookies, requires https
+}
 
-//     const db = client.db(dbName);
+app.use(session(sess));
 
-//     // app.post('/addwow', function (req, res) {
-//     //     var newWow = req.body.newwow;
-//     //     db.collection('wow').insertOne({text: newWow, giver: 'aweinstein@vacayhomeconnect.com', receiver: 'zkann@vacayhomeconnect.com'});
-//     //     res.redirect('/');
-//     // });
+app.use(passport.initialize());
+app.use(passport.session());
 
-//     // app.get('/', function(req, res) {
-//     //     db.collection('wow').find({}).toArray(function(err, result) {
-//     //         if (err) {
-//     //             throw err;
-//     //             return res.status(400).send(err);
-//     //         }
-//     //         res.render('index', { wow: result });
-//     //     });
-//     // });
-// });
+// app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Handle auth failure error messages
+app.use(function (req, res, next) {
+    if (req && req.query && req.query.error) {
+        req.flash('error', req.query.error);
+    }
+    if (req && req.query && req.query.error_description) {
+        req.flash('error_description', req.query.error_description);
+    }
+    next();
+});
+
+app.use(userInViews());
+app.use('/', authRouter);
+app.use('/', indexRouter);
+app.use('/', usersRouter);
+
+// Catch 404 and forward to error handler
+app.use(function (req, res, next) {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+// Error handlers
+
+// Development error handler
+// Will print stacktrace
+if (app.get('env') === 'development') {
+    app.use(function (err, req, res, next) {
+        res.status(err.status || 500);
+        res.render('error', {
+        message: err.message,
+        error: err
+        });
+    });
+}
+
+// Production error handler
+// No stacktraces leaked to user
+app.use(function (err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+        message: err.message,
+        error: {}
+    });
+});
 
 // the server is listening on port 3000 for connections
 app.listen(3000, function (err) {
@@ -50,12 +125,6 @@ app.listen(3000, function (err) {
 	}
 
 	require('./utils/db');
-
-    // loop through all routes and dynamically require them – passing api
-    fs.readdirSync(path.join(__dirname, 'routes')).map(file => {
-        console.log(file);
-        require('./routes/' + file)(app);
-    });
 });
 
 module.exports = app;
